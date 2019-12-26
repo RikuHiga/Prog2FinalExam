ed3625fe7a38a0709dce470b251e4e00a388fbe4
Riku Higa <e195760@ie.u-ryukyu.ac.jp>
2019-12-26 11:14:08 +0900

    UnitTest,jar,JavaDocの動作確認終了

---
+++ src/main/java/jp/ac/uryukyu/ie/e195760/main.java
@@ -0,0 +1,18 @@
+package jp.ac.uryukyu.ie.e195760;
+//テスト完了,jar完了,
+public class main {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        System.out.println(add(64,128));
+    }
+
+    /**
+     * 足算するメソッド
+     * @param a　引数一つ目
+     * @param b 引数二つ目
+     * @return 演算結果を返す
+     */
+    public static int add(int a,int b){
+        return a+b;
+    }
+}
---
+++ src/test/java/mainTest.java
@@ -0,0 +1,12 @@
+import jp.ac.uryukyu.ie.e195760.main;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class mainTest {
+
+    @Test
+    void add() {
+        assertEquals(main.add(3,5),3+5);
+    }
+}
