a522e8e806663cd2787d36d785a9ff675f2e8066
Riku Higa <e195760@ie.u-ryukyu.ac.jp>
2020-01-05 20:00:36 +0900

    editCommandにtry catchを追加

--- src/main/java/jp/ac/uryukyu/ie/e195760/editCommand.java
+++ src/main/java/jp/ac/uryukyu/ie/e195760/editCommand.java
@@ -9,32 +9,37 @@ public class editCommand {
         String[] todo;
         String[] splitCmd;
         //指定されたtodoを持ってくる
-        todo=TodoManager.todoList.get(TodoManager.visibleList.get(Integer.parseInt(cmd[1])-1));
-        //cmd[2]を","で分割する
-        splitCmd=cmd[2].split(",");
-        for(String config:splitCmd){//config:title=example
-            //コマンドを一個ずつ、持ってきたtodoに適用する
-            if(config.indexOf(Main.TITLE[0])==0){
-                todo[Integer.parseInt(Main.TITLE[2])]=config.split(Main.TITLE[0])[1];
-            }else if(config.indexOf(Main.DESCRIPTION[0])==0){
-                todo[Integer.parseInt(Main.DESCRIPTION[2])]=config.split(Main.DESCRIPTION[0])[1];
-            }else if(config.indexOf(Main.DEADLINE[0])==0){
-                todo[Integer.parseInt(Main.DEADLINE[2])]=config.split(Main.DEADLINE[0])[1];
-            }else if(config.indexOf(Main.TAG[0])==0){
-                todo[Integer.parseInt(Main.TAG[2])]=config.split(Main.TAG[0])[1];
-            }else if(config.indexOf(Main.DURATION[0])==0){
-                todo[Integer.parseInt(Main.DURATION[2])]=config.split(Main.DURATION[0])[1];
-            }else if(config.equals(Main.PRIORITY[0])){
-                if(todo[Integer.parseInt(Main.PRIORITY[2])].equals("true")){
-                    todo[Integer.parseInt(Main.PRIORITY[2])]="false";
+        try{
+            todo=TodoManager.todoList.get(TodoManager.visibleList.get(Integer.parseInt(cmd[1])-1));
+            //cmd[2]を","で分割する
+            splitCmd=cmd[2].split(",");
+            for(String config:splitCmd){//config:title=example
+                //コマンドを一個ずつ、持ってきたtodoに適用する
+                if(config.indexOf(Main.TITLE[0])==0){
+                    todo[Integer.parseInt(Main.TITLE[2])]=config.split(Main.TITLE[0])[1];
+                }else if(config.indexOf(Main.DESCRIPTION[0])==0){
+                    todo[Integer.parseInt(Main.DESCRIPTION[2])]=config.split(Main.DESCRIPTION[0])[1];
+                }else if(config.indexOf(Main.DEADLINE[0])==0){
+                    todo[Integer.parseInt(Main.DEADLINE[2])]=config.split(Main.DEADLINE[0])[1];
+                }else if(config.indexOf(Main.TAG[0])==0){
+                    todo[Integer.parseInt(Main.TAG[2])]=config.split(Main.TAG[0])[1];
+                }else if(config.indexOf(Main.DURATION[0])==0){
+                    todo[Integer.parseInt(Main.DURATION[2])]=config.split(Main.DURATION[0])[1];
+                }else if(config.equals(Main.PRIORITY[0])){
+                    if(todo[Integer.parseInt(Main.PRIORITY[2])].equals("true")){
+                        todo[Integer.parseInt(Main.PRIORITY[2])]="false";
+                    }else{
+                        todo[Integer.parseInt(Main.PRIORITY[2])]="true";
+                    }
                 }else{
-                    todo[Integer.parseInt(Main.PRIORITY[2])]="true";
+                    System.out.println(config+"は不明なコマンドです。");
                 }
-            }else{
-                System.out.println(config+"は不明なコマンドです。");
+                //変更したtodoを元の場所に戻す
             }
-            //変更したtodoを元の場所に戻す
+            TodoManager.todoList.set(TodoManager.visibleList.get(Integer.parseInt(cmd[1])-1),todo);
+        }catch(NumberFormatException ex){
+            System.out.println("数値を指定してください。");
         }
-        TodoManager.todoList.set(TodoManager.visibleList.get(Integer.parseInt(cmd[1])-1),todo);
+
     }
 }
